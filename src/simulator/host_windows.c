f404909e10650b56c5f55fbdc7b7ada4a055fa9f
vKrls <89294878+vKrls@users.noreply.github.com>
2026-07-05 05:01:05 -0500

    Compatibilidad

--- src/simulator/host_windows.c
+++ src/simulator/host_windows.c
@@ -6,13 +6,85 @@
 #include <conio.h>
 #include <windows.h>
 
+#ifndef CREATE_WAITABLE_TIMER_HIGH_RESOLUTION
+#define CREATE_WAITABLE_TIMER_HIGH_RESOLUTION 0x00000002
+#endif
+
+#define HUNDRED_NS_PER_US 10LL
+#define SPIN_TAIL_US 500U
+
+static LARGE_INTEGER performance_frequency(void)
+{
+	static LARGE_INTEGER frequency;
+
+	if (frequency.QuadPart == 0)
+		QueryPerformanceFrequency(&frequency);
+	return frequency;
+}
+
+static LONGLONG performance_counter(void)
+{
+	LARGE_INTEGER counter;
+
+	QueryPerformanceCounter(&counter);
+	return counter.QuadPart;
+}
+
+static LONGLONG microseconds_to_ticks(unsigned int microseconds)
+{
+	LARGE_INTEGER frequency = performance_frequency();
+
+	return (LONGLONG)microseconds * frequency.QuadPart / 1000000LL;
+}
+
+static HANDLE high_resolution_timer(void)
+{
+	static HANDLE timer;
+	static BOOL initialized;
+
+	if (!initialized) {
+		timer = CreateWaitableTimerExW(
+			NULL, NULL, CREATE_WAITABLE_TIMER_HIGH_RESOLUTION,
+			SYNCHRONIZE | TIMER_MODIFY_STATE);
+		if (timer == NULL)
+			timer = CreateWaitableTimerW(NULL, TRUE, NULL);
+		initialized = TRUE;
+	}
+	return timer;
+}
+
+static void wait_with_timer(unsigned int microseconds)
+{
+	HANDLE timer = high_resolution_timer();
+	LARGE_INTEGER due_time;
+
+	if (timer == NULL) {
+		Sleep(microseconds / 1000U);
+		return;
+	}
+
+	due_time.QuadPart = -((LONGLONG)microseconds * HUNDRED_NS_PER_US);
+	if (SetWaitableTimer(timer, &due_time, 0, NULL, NULL, FALSE))
+		WaitForSingleObject(timer, INFINITE);
+}
+
+static void spin_until(LONGLONG target)
+{
+	while (performance_counter() < target)
+		YieldProcessor();
+}
+
 void sleep_us(unsigned int microseconds)
 {
-	DWORD milliseconds = (microseconds + 999U) / 1000U;
+	LONGLONG target;
 
-	if (milliseconds == 0U && microseconds > 0U)
-		milliseconds = 1U;
-	Sleep(milliseconds);
+	if (microseconds == 0U)
+		return;
+
+	target = performance_counter() + microseconds_to_ticks(microseconds);
+	if (microseconds > SPIN_TAIL_US)
+		wait_with_timer(microseconds - SPIN_TAIL_US);
+	spin_until(target);
 }
 
 bool stdin_has_data(void)
@@ -41,10 +113,10 @@ int64_t monotonic_ms(void)
 	LARGE_INTEGER counter;
 
 	if (!has_frequency) {
-		QueryPerformanceFrequency(&frequency);
+		frequency = performance_frequency();
 		has_frequency = TRUE;
 	}
-	QueryPerformanceCounter(&counter);
+	counter.QuadPart = performance_counter();
 	return (int64_t)(counter.QuadPart * 1000 / frequency.QuadPart);
 }
 
